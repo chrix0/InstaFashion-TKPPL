bd5b42f684fd4467346ad028c0166f1a69a7d140
chrix0 <christiantopratama@gmail.com>
2023-01-23 15:57:20 +0700

    Transaction History

--- app/src/main/java/com/PisangHitam/InstaFashion/shop_tracker.kt
+++ app/src/main/java/com/PisangHitam/InstaFashion/shop_tracker.kt
@@ -9,14 +9,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_shop_product_list.*
 import kotlinx.android.synthetic.main.activity_shop_tracker.*
+import org.jetbrains.anko.startActivity
 import org.w3c.dom.Text
 
 class shop_tracker : AppCompatActivity() {
-    //Alert dialog details ok
-    //Ignore back button ok
-    //Alert dialog cancel ok
-    //checkout fill all checker
-    //Multi fab library
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +20,11 @@ class shop_tracker : AppCompatActivity() {
 
         val actionbar = supportActionBar
         actionbar!!.title = getString(R.string.purchase_history_title)
+        actionbar.setDisplayHomeAsUpEnabled(true)
 
-        var adapter = recycler_shoptransaction_adapter(this, singletonData.accList[singletonData.currentAccId].transactionHistory){
+        var user = singletonData.getCurUserObj(this)
+
+        var adapter = recycler_shoptransaction_adapter(this, user!!.transactionHistory){
             var layout = layoutInflater.inflate(R.layout.dialog_shop_history_details,null)
             var dialog = AlertDialog.Builder(this).apply{
                 setView(layout)
@@ -45,7 +44,7 @@ class shop_tracker : AppCompatActivity() {
             var close = layout.findViewById<TextView>(R.id.closeDetails)
 
             id.setText(it.id.toString())
-            username.setText(singletonData.accList[singletonData.currentAccId].userName)
+            username.setText(user!!.userName)
             datetime.setText(it.datePurchase)
             method.setText(it.method)
             address.setText(singletonData.formatAlamat(it.address))
@@ -64,8 +63,12 @@ class shop_tracker : AppCompatActivity() {
 
         returnShop.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
-            intent.putExtra(RETURN_LAST_TAB, "SHOP")
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
